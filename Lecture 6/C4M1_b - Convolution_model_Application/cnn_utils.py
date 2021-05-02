fccccc1ee900eee8f3496aeee76238b8a6e9ac27
Fabian Toledo <fabian.alejandro.toledo@gmail.com>
2021-05-02 11:36:59 -0300

    Finished Programming Assignment C4M1 - Convolution model Application Added some extra practice

--- Lecture 6/C4M1_b - Convolution_model_Application/cnn_utils.py
+++ Lecture 6/C4M1_b - Convolution_model_Application/cnn_utils.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import tensorflow.compat.v1 as tf #import tensorflow as tf # As I'm using tensorFlow 2, import v1 compatibility
 from tensorflow.python.framework import ops
 
 def load_dataset():
@@ -68,88 +68,59 @@ def convert_to_one_hot(Y, C):
     return Y
 
 
-def forward_propagation_for_predict(X, parameters):
-    """
-    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
-    
-    Arguments:
-    X -- input dataset placeholder, of shape (input size, number of examples)
-    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
-                  the shapes are given in initialize_parameters
 
-    Returns:
-    Z3 -- the output of the last LINEAR unit
-    """
+# def forward_propagation_for_predict(X, parameters):
+#     """
+#     Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
     
-    # Retrieve the parameters from the dictionary "parameters" 
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-    W2 = parameters['W2']
-    b2 = parameters['b2']
-    W3 = parameters['W3']
-    b3 = parameters['b3'] 
-                                                           # Numpy Equivalents:
-    Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
-    A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
-    Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, a1) + b2
-    A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
-    Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3,Z2) + b3
-    
-    return Z3
+#     Arguments:
+#     X -- input dataset placeholder, of shape (input size, number of examples)
+#     parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+#                   the shapes are given in initialize_parameters
 
-def predict(X, parameters):
+#     Returns:
+#     Z3 -- the output of the last LINEAR unit
+#     """
     
-    W1 = tf.convert_to_tensor(parameters["W1"])
-    b1 = tf.convert_to_tensor(parameters["b1"])
-    W2 = tf.convert_to_tensor(parameters["W2"])
-    b2 = tf.convert_to_tensor(parameters["b2"])
-    W3 = tf.convert_to_tensor(parameters["W3"])
-    b3 = tf.convert_to_tensor(parameters["b3"])
+#     # Retrieve the parameters from the dictionary "parameters" 
+#     W1 = parameters['W1']
+#     b1 = parameters['b1']
+#     W2 = parameters['W2']
+#     b2 = parameters['b2']
+#     W3 = parameters['W3']
+#     b3 = parameters['b3'] 
+#                                                            # Numpy Equivalents:
+#     Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
+#     A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
+#     Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, a1) + b2
+#     A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
+#     Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3,Z2) + b3
     
-    params = {"W1": W1,
-              "b1": b1,
-              "W2": W2,
-              "b2": b2,
-              "W3": W3,
-              "b3": b3}
+#     return Z3
+
+# def predict(X, parameters):
     
-    x = tf.placeholder("float", [12288, 1])
+#     W1 = tf.convert_to_tensor(parameters["W1"])
+#     b1 = tf.convert_to_tensor(parameters["b1"])
+#     W2 = tf.convert_to_tensor(parameters["W2"])
+#     b2 = tf.convert_to_tensor(parameters["b2"])
+#     W3 = tf.convert_to_tensor(parameters["W3"])
+#     b3 = tf.convert_to_tensor(parameters["b3"])
     
-    z3 = forward_propagation_for_predict(x, params)
-    p = tf.argmax(z3)
+#     params = {"W1": W1,
+#               "b1": b1,
+#               "W2": W2,
+#               "b2": b2,
+#               "W3": W3,
+#               "b3": b3}
     
-    sess = tf.Session()
-    prediction = sess.run(p, feed_dict = {x: X})
+#     x = tf.placeholder("float", [12288, 1])
+    
+#     z3 = forward_propagation_for_predict(x, params)
+#     p = tf.argmax(z3)
+    
+#     sess = tf.Session()
+#     prediction = sess.run(p, feed_dict = {x: X})
         
-    return prediction
+#     return prediction
 
-#def predict(X, parameters):
-#    
-#    W1 = tf.convert_to_tensor(parameters["W1"])
-#    b1 = tf.convert_to_tensor(parameters["b1"])
-#    W2 = tf.convert_to_tensor(parameters["W2"])
-#    b2 = tf.convert_to_tensor(parameters["b2"])
-##    W3 = tf.convert_to_tensor(parameters["W3"])
-##    b3 = tf.convert_to_tensor(parameters["b3"])
-#    
-##    params = {"W1": W1,
-##              "b1": b1,
-##              "W2": W2,
-##              "b2": b2,
-##              "W3": W3,
-##              "b3": b3}
-#
-#    params = {"W1": W1,
-#              "b1": b1,
-#              "W2": W2,
-#              "b2": b2}    
-#    
-#    x = tf.placeholder("float", [12288, 1])
-#    
-#    z3 = forward_propagation(x, params)
-#    p = tf.argmax(z3)
-#    
-#    with tf.Session() as sess:
-#        prediction = sess.run(p, feed_dict = {x: X})
-#        
-#    return prediction
